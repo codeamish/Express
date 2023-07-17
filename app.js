8cddf58583821af3ce1f374997ef5ed2f5556a21
Amish <amishguleria9977@gmail.com>
2023-07-16 18:36:08 -0700

    Added Data sanitization

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet'); // set security HTTP headers
-
+const mongoSanitize = require('express-mongo-sanitize'); // Data Sanitization against NoSQL query injection
+const xss = require('xss-clean'); // Data Sanitization against XSS
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +34,12 @@ app.use('/api',limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({limit : '10kb'}));
 
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
