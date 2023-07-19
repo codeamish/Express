93e453595fae3773a02785700029f60abfad235d
Amish <amishguleria9977@gmail.com>
2023-07-18 22:06:15 -0700

    Added some authorization logic

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router({ mergeParams: true });
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
+router.use(authController.protect);
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourAndUserIds,
     reviewController.createReview
@@ -15,7 +15,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .delete(reviewController.deleteReview)
-  .patch(reviewController.updateReview);
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  )
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  );
 
-module.exports = router;
+module.exports = router; 
