e92a6f35f7bf9af30f8694b51b503d0ca6151059
Amish <amishguleria9977@gmail.com>
2023-08-01 19:14:45 +0530

    Made the tour page

---
+++ routes/viewRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const viewsController = require('../controllers/viewsController');
+
+
+router.get('/', viewsController.getOverview);
+
+router.get('/tour/:slug',viewsController.getTour);
+
+module.exports = router;
